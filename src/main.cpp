c54f6b061d9afe4dab820656d679f21f81fb62c4
heloabrantessb <heloisa.abrantesdev@gmail.com>
2025-12-02 10:59:49 -0300

    Refactor platformio.ini and main.cpp for ESP32 configuration and Bluetooth functionality

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,41 @@ BluetoothSerial SerialBT;
 
 int valor = 0;
 
-void setup() {
+int motorPin = 5;
+
+void setup()
+{
   Serial.begin(115200);
   delay(1000);
 
-  if(!SerialBT.begin("ESP32_vibracall")) {
+  if (!SerialBT.begin("ESP32_vibracall"))
+  {
     Serial.println("An error occurred initializing Bluetooth");
-  } else {
+  }
+  else
+  {
     Serial.println("Bluetooth initialized");
   }
+
+  pinMode(motorPin, OUTPUT);
 }
 
+void loop()
+{
+  if (SerialBT.available())
+  {
+    String s = SerialBT.readStringUntil('\n');
+    s.trim();
 
-void loop() {}
+    if (s.length() > 0)
+    {
+      int valor = s.toInt();
+      valor = constrain(valor, 0, 255);
+
+      analogWrite(motorPin, valor);
+
+      Serial.print("Recebido via BT: ");
+      Serial.println(valor);
+    }
+  }
+}
